9407ecae4d9c400d165f2e8db58ba245562e754e
wellingtonsalatta <wellington.salatta@bitzen.com.br>
2021-07-11 13:15:28 -0300

    Criados métodos de pesquisa de transportadoras.

--- app/Http/Controllers/TransportadoraController.php
+++ app/Http/Controllers/TransportadoraController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Transportadora;
+use App\Services\TransportadoraService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Exception;
 
 class TransportadoraController extends Controller
 {
@@ -14,28 +17,12 @@ class TransportadoraController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        try {
+            $response = (new TransportadoraService())->list();
+            return response($response, Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response($e->getMessage(), $e->getCode());
+        }
     }
 
     /**
@@ -46,40 +33,11 @@ class TransportadoraController extends Controller
      */
     public function show(Transportadora $transportadora)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Transportadora  $transportadora
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Transportadora $transportadora)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Transportadora  $transportadora
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Transportadora $transportadora)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Transportadora  $transportadora
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Transportadora $transportadora)
-    {
-        //
+        try {
+            $response = (new TransportadoraService())->show($transportadora);
+            return response($response, Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response($e->getMessage(), $e->getCode());
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::get('/transportadora', 'App\Http\Controllers\TransportadoraController@index');
+Route::get('/transportadora/{transportadora}', 'App\Http\Controllers\TransportadoraController@show');
